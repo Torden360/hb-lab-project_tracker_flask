ee9e5e60c647e8e97faf0850cd48b5ee5955d356
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-31 17:27:22 -0700

    finished adding form to get student name

--- hackbright_web.py
+++ hackbright_web.py
@@ -6,6 +6,12 @@ import hackbright
 
 app = Flask(__name__)
 
+@app.route("/")
+def get_info():
+
+
+    return render_template("new_student.html")
+
 
 @app.route("/student")
 def get_student():
@@ -24,7 +30,18 @@ def get_student_form():
     """Show form for searching for a student."""
 
     return render_template("student_search.html")
-    
+
+@app.route("/student-add", methods=['POST'])
+def student_add():
+    """Add a student."""
+    new_student = request.form.get('new_name')
+
+
+
+    return render_template("added_student.html", new_student=new_student)
+
+
+
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
